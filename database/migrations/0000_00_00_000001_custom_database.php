835303e7d0c0d2b75e6dc70f8b7d4f588d69b2db
@JosuE- <josuandroidg7@gmail.com>
2023-04-26 22:09:48 -0400

    Integrity

--- database/migrations/0000_00_00_000001_custom_database.php
+++ database/migrations/0000_00_00_000001_custom_database.php
@@ -29,9 +29,12 @@ class CustomDatabase extends Migration
             $table->increments('id');
             $table->string('zip_code')->nullable();
             $table->string('locality')->nullable();
-            $table->integer('federal_entity_id')->nullable();
-            $table->integer('municipality_id')->nullable();
+            $table->integer('federal_entity_id')->unsigned();
+            $table->integer('municipality_id')->unsigned();
             $table->timestamps();
+            $table->foreign('federal_entity_id')->references('id')->on('federal_entities')->onDelete('restrict');
+            $table->foreign('municipality_id')->references('id')->on('municipalities')->onDelete('restrict');
+            $table->index(['federal_entity_id', 'municipality_id']);
         });
         Schema::create('settlement_types', function (Blueprint $table) {
             $table->increments('id');
@@ -40,12 +43,15 @@ class CustomDatabase extends Migration
         });
         Schema::create('settlements', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('zip_code_id')->nullable();
+            $table->integer('zip_code_id')->unsigned();
             $table->integer('key')->nullable();
             $table->string('name')->nullable();
             $table->string('zone_type')->nullable();
-            $table->integer('settlement_type_id')->nullable();
+            $table->integer('settlement_type_id')->unsigned();
             $table->timestamps();
+            $table->foreign('zip_code_id')->references('id')->on('zip_codes')->onDelete('restrict');
+            $table->foreign('settlement_type_id')->references('id')->on('settlement_types')->onDelete('restrict');
+            $table->index(['zip_code_id', 'settlement_type_id']);
         });
     }
     /**
